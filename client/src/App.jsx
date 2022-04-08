a3d3bd30cd25e7083b346ea4a11868ceb19f440b
Eric Noubissi <noubissi10@yahoo.fr>
2022-04-08 06:19:05 +0200

    Utilisation de react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,28 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 
 const App = () => {
-  return <Home/>;
+  const user = true;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/products/:category" element={<ProductList/>} />
+        <Route path="/product/:id" element={<ProductList/>} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/register" element={user ? <Navigate to="/"/> : <Register />} />
+        <Route path="/login" element={user ? <Navigate to="/"/> : <Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
